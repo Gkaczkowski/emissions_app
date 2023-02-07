0210df7d13f60fa596dca93d2dfaaf614dd59e4d
gkaczkowski <gkaczkowski@tesla.com>
2023-02-07 11:42:01 -0800

    add more plots

--- App/emissions_app.py
+++ App/emissions_app.py
@@ -3,6 +3,8 @@ import pandas as pd
 import snowflake.connector
 import plotly.graph_objects as go
 
+from statsmodels.tsa.seasonal import MSTL
+
 st.title('Carbon Intensity Comparison')
 
 
@@ -50,7 +52,7 @@ def load_data_2():
     return df2
 
 
-@st.cache(ttl=24 * 60 * 60)
+@st.cache(ttl=24 * 60 * 60, show_spinner=False)
 def aggregate_data(df1, df2):
     df3 = pd.concat([df1, df2], copy=False).sort_values(by='datetime')
     df3.index = pd.to_datetime(df3.index, utc=True)
@@ -59,11 +61,31 @@ def aggregate_data(df1, df2):
     return df3
 
 
-@st.cache(ttl=24 * 60 * 60)
+@st.cache(ttl=24 * 60 * 60, show_spinner=False)
 def get_aggregated_data(data, target: str):
     df = data.groupby(pd.Grouper(freq=target)).mean()
     df['delta_marginal_vs_average_tons_per_mwh'] = df['moer_tons_per_mwh'] - df['carbon_intensity_tons_per_mwh']
     return df
+
+
+@st.cache(ttl=24 * 60 * 60, show_spinner=False)
+def get_more_data(data, target: str):
+    fig = go.Figure()
+    df_list = []
+    for ts, df in data.groupby(pd.Grouper(freq=target)):
+        # df_list.append(df.reset_index()['moer_tons_per_mwh'])
+        # df = df.reset_index()["moer_tons_per_mwh"]
+        try:
+            fig.add_trace(go.Scatter(x=df.index, y=df['moer_tons_per_mwh'], opacity=0.1))
+        except KeyError:
+            pass
+    df2 = data.groupby(pd.Grouper(freq=target)).mean()
+    fig.add_trace(go.Scatter(x=df2.index, y=df2['moer_tons_per_mwh'], name='mean'))
+    fig.update_layout(title=f"Marginal Emissions Rate in California by {target}")
+    fig.update_xaxes(title_text='Time Frame')
+    fig.update_yaxes(title_text="Marginal Emissions Rate (t/MWh)")
+
+    return fig
 
 
 data_load_state = st.text('Loading data...')
@@ -96,14 +118,8 @@ fig.add_trace(go.Scatter(x=time_data.index, y=time_data['moer_tons_per_mwh'], na
 fig.add_trace(go.Scatter(x=time_data.index, y=time_data['carbon_intensity_tons_per_mwh'],
                          name='carbon_intensity_tons_per_mwh',
                          fill='tozeroy', mode='none'))
-fig.update_xaxes(
-    title_text='Time Frame',
-    # title_font=dict(size=16, family='Verdana', color='black'))
-)
-fig.update_yaxes(
-    title_text="tons/mWh",
-    # title_font=dict(size=30, family='Verdana', color='black'))
-)
+fig.update_xaxes(title_text='Time Frame')
+fig.update_yaxes(title_text="tons/MWh")
 fig.update_layout(legend=dict(
     orientation="h",
     yanchor="top",
@@ -126,3 +142,6 @@ option_1 = st.selectbox(
     ('Week', 'Month', 'Year'), key='2')
 time_data = get_aggregated_data(data, option_map[option_1])
 st.area_chart(time_data, y=['delta_marginal_vs_average_tons_per_mwh'])
+
+mean_vs_all_plot = get_more_data(data, option_map[option_1])
+st.plotly_chart(mean_vs_all_plot)
